b5dfa3977cb970156c7b3389206df3f2bb91b5a1
trandinhchuong00 <trandinhchuong00@gmail.com>
2021-12-12 17:54:52 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+botname='ChatBot'
+chatbot = ChatBot(botname, 
+	logic_adapters=[
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'I am sorry, but I do not understand.',
+            'maximum_similarity_threshold': 0.8
+        }
+    ],
+ )
+
+@app.route("/")
+def home():
+    return render_template("index.html", botname=botname)
+
+@app.route("/get")
+def get_bot_response():
+	userInput=request.args.get('msg')
+	return str(chatbot.get_response(userInput))
+
+if __name__ == '__main__':
+	app.run(port=5500)
